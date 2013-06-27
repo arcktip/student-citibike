d677b7e5f939509f3edfe0a876e013bded45b75a
ashleygwilliams <heyashleyashley@gmail.com>
2013-06-26 20:01:30 -0400

    initial commit

---
+++ app.rb
@@ -0,0 +1,14 @@
+require 'rubygems'
+require 'bundler'
+
+Bundler.require
+
+Dir.glob('./lib/*.rb') do |model|
+  require model
+end
+
+module Citibike
+	class App < Sinatra::Application
+
+	end
+end
